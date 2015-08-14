eed1e66614fa6827a299b058dfb878cf910a9ade
denisreis <dpcr@cin.ufpr.br>
2015-08-13 21:27:06 -0300

    nascimento

--- grails-app/domain/teste02/Denisreis.groovy
+++ grails-app/domain/teste02/Denisreis.groovy
@@ -3,9 +3,11 @@ package teste02
 class Denisreis {
 	String nome
 	String cpf
+	Integer nascimento
 			static constraints = {
 				nome nullable:false, blank:false
 				cpf nullable:false, blank:false
+				nascimento nullable:false, blank:false
 			}
 		}
 		
